10104a0a24fe70dfaed7c6a6697d9592062b88cb
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-02-24 19:03:03 +0530

    create private fields:fName,lName and phoneNumber

--- src/src/main/Main.java
+++ src/src/main/Main.java
@@ -1,10 +1,19 @@
 package main;
 
+import defination.SinglyLinkedList;
+import person.Person;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        SinglyLinkedList<Person> list = new SinglyLinkedList<>();
+        boolean flag = true;
+
     }
 
 }
--- src/src/person/Person.java
+++ src/src/person/Person.java
@@ -1,4 +1,8 @@
 package person;
 
 public class Person {
+    private String fName;
+    private String lName;
+    private String email;
+    private long phoneNumber;
 }
