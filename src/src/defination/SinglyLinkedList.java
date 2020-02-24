515276da0a26806d9353731a43c6686ac0658971
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-02-24 17:56:08 +0530

    create getter method to access the data

--- src/src/defination/SinglyLinkedList.java
+++ src/src/defination/SinglyLinkedList.java
@@ -32,6 +32,14 @@ public class SinglyLinkedList<E> implements SinglyADT<E> {
         private E data;
         private Node<E> next;
 
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
         public Node(E data, Node<E> next) {
             this.data = data;
             this.next = next;
