d8d692fee35cec8e32c471477bfd94fd98436afe
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-02-24 19:04:45 +0530

    create Person class constructor

--- src/src/person/Person.java
+++ src/src/person/Person.java
@@ -1,8 +1,23 @@
 package person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
     private String fName;
     private String lName;
     private String email;
     private long phoneNumber;
+    List<Long> list = new ArrayList<>();
+
+    public Person() {
+    }
+
+    public Person(String fName, String lName, String email, long phoneNumber, List<Long> list) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.list = list;
+    }
 }
